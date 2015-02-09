2f46d9677cc1632e3a768a152b26698f359ac6d7
Greg Mallett <gregory.mallett@nytimes.com>
2015-02-08 19:36:19 -0500

    Added action bar

--- main/src/main/java/com/helloapp/main/DisplayMessageActivity.java
+++ main/src/main/java/com/helloapp/main/DisplayMessageActivity.java
@@ -2,6 +2,10 @@ package com.helloapp.main;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends Activity {
@@ -11,7 +15,29 @@ public class DisplayMessageActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.displaymessageactivity);
 
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
         TextView messageText = (TextView) findViewById(R.id.message_messageText);
         messageText.setText(getIntent().getStringExtra("MESSAGE"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- main/src/main/java/com/helloapp/main/MyActivity.java
+++ main/src/main/java/com/helloapp/main/MyActivity.java
@@ -3,6 +3,9 @@ package com.helloapp.main;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -21,6 +24,26 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        CheckBox randomCheckBox = (CheckBox) findViewById(R.id.random);
+        randomCheckBox.setChecked(random);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void sendMessage(View view) {
